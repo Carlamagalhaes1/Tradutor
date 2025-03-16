3d6099033567640af6395a6dfd1e81460e1baddd
Carla Magalhães <carlamagalhes60@gmail.com>
2025-03-16 13:47:58 -0300

    estilos do header e main

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,24 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>tardutor</h1>
+      <div className="min-h-screen bg-backgroud flex flex-col ">
+
+        <header className="bg-white shadow-sm">
+          <div className="max-w-5xl mx-auto px-4 py-3 flex items-center">
+            <h1 className="text-headerColor text-2xl font-bold">Tradutor</h1>
+          </div>
+        </header>
+        
+        <main className="flex-grow flex items-start justify-center px-4 py-8">
+          <div className="w-full max-w-5xl bg-white rounded-lg shadow-md overflow-hidden-">
+            <div
+            className="flex items-center justify-between p-4 border-b border-gray-200"></div>
+          </div>
+
+        </main>
+
+        
+        
       </div>
        
     </>
