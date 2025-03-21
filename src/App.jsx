23f22abc25b941ff3ded7f866e2dda72a78faf9f
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-03-21 14:53:02 -0300

    teste1

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ function App() {
 
         <footer className="bg-white border-t border-gray-200 mt-auto ">
           <div className="max-w-5xl mx-auto px-4 py-3 text-sm text-headerColor">
-            &copy; {new Date().getFullYear()} Tradutorr
+            &copy; {new Date().getFullYear()} Tradutorrttt
           </div>
         </footer>
       </div>
