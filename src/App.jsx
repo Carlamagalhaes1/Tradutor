a9d50e11b54e7cb733bbbcf23ded5919fbda4dfe
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-05-21 13:57:13 -0300

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ function App() {
 
         <footer className="bg-white border-t border-gray-200 mt-auto ">
           <div className="max-w-5xl mx-auto px-4 py-3 text-sm text-headerColor">
-            &copy; {new Date().getFullYear()} Tradutorrttt
+            &copy; {new Date().getFullYear()} Tradutor
           </div>
         </footer>
       </div>
